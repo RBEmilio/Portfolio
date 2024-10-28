8f432f346d81db3662dbda38e8f3bc5805faf0eb
Emilio Reina Borrego <123060864+RBEmilio@users.noreply.github.com>
2024-10-28 11:24:09 +0100

    All pages
    All pages content

---
+++ src/app/abou-me/abou-me.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-abou-me',
+  standalone: true,
+  imports: [],
+  templateUrl: './abou-me.component.html',
+  styleUrl: './abou-me.component.css'
+})
+export class AbouMeComponent {
+
+}
